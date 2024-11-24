460836009fa8961ba71baae267bf7d6ce89122d4
RajaOX <raja.koduri@mihira.ai>
2024-11-24 15:17:59 -0800

    Update torchure.py
    Fixed xpu.is_available

--- torchure.py
+++ torchure.py
@@ -17,7 +17,7 @@ def get_device():
         return 'cuda'
     elif torch.backends.mps.is_available():
         return 'mps'
-    elif hasattr(torch.backends, 'xpu') and torch.backends.xpu.is_available():
+    elif torch.xpu.is_available():
         return 'xpu'
     else:
         return 'cpu'
